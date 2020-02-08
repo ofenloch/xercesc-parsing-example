cf25461c6048599f73ec52b08e5817088210ee0d
Oliver Ofenloch <oliver.ofenloch@email.de>
2020-02-08 17:06:04 +0100

    add basic xml parsing to main, adjust CMakeList.txt to get the xerces-c stuff rigth, add sample daata to test parsing

--- bin/include/main.h
+++ bin/include/main.h
@@ -1,6 +1,21 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+
+#include <xercesc/util/PlatformUtils.hpp>
+#include <xercesc/util/XMLString.hpp>
+#include <xercesc/sax2/XMLReaderFactory.hpp>
+#include <xercesc/sax2/SAX2XMLReader.hpp>
+
+#include <xercesc/parsers/XercesDOMParser.hpp>
+#include <xercesc/dom/DOM.hpp>
+#include <xercesc/sax/HandlerBase.hpp>
+#include <xercesc/util/PlatformUtils.hpp>
+
+
 #include <mainlib.h>
 
 #endif // #ifndef _MAIN_H_
--- bin/source/main.cpp
+++ bin/source/main.cpp
@@ -7,6 +7,93 @@ int main(int argc, char *argv[])
   myGreeter.sayHello();
 
   myGreeter.sayHello("Hello, World!");
-  
+
+  if (argc <= 1)
+  {
+    std::cout << "Usage: " << argv[0] << " <xml file>" << std::endl
+              << std::endl;
+    std::cout << "       "
+              << "The <xml file> is mandatory." << std::endl;
+    return 1;
+  }
+  std::string filename = argv[1];
+
+  // Independent of the API you want to use, DOM, SAX, or SAX2, your application
+  // must initialize the Xerces system before using the API, and terminate it
+  // after you are done. This is achieved by the following code:
+  try
+  {
+    xercesc::XMLPlatformUtils::Initialize();
+    // Do not forget to call
+    //    xercesc::XMLPlatformUtils::Terminate();
+    // when you're done.
+  }
+  catch (const xercesc::XMLException &toCatch)
+  {
+    // Do your failure processing here
+    return 1;
+  }
+
+  xercesc::XercesDOMParser *m_parser = new xercesc::XercesDOMParser();
+  m_parser->setValidationScheme(xercesc::XercesDOMParser::Val_Always);
+  m_parser->setDoNamespaces(true); // optional
+  xercesc::ErrorHandler *m_errorHandler = (xercesc::ErrorHandler *)new xercesc::HandlerBase();
+  m_parser->setErrorHandler(m_errorHandler);
+
+  xercesc::DOMDocument *m_document = nullptr;
+
+  try
+  {
+    m_parser->parse(filename.c_str());
+    m_document = m_parser->getDocument();
+    std::cout << "Parsed file \"" << filename << "\" :-))" << std::endl;
+    xercesc::DOMDocumentType *docType = m_document->getDoctype();
+    char *message = xercesc::XMLString::transcode(docType->getName());
+    std::cout << "DOMDocumentType is \"" << message << "\"" << std::endl;
+    xercesc::DOMNodeList *children = m_document->getChildNodes();
+    if (children != nullptr)
+    {
+      int nChildren = children->getLength();
+      std::cout << "document has " << nChildren << " child node(s):" << std::endl;
+      xercesc::XMLString::release(&message);
+      for (int i = 0; i < nChildren; i++)
+      {
+        xercesc::DOMNode *child = children->item(i);
+        std::cout << "  " << i + 1 << " : " << xercesc::XMLString::transcode(child->getNodeName()) << std::endl;
+      }
+    }
+    else
+    {
+      std::cout << "document has no  child nodes" << std::endl;
+    }
+  }
+  catch (const xercesc::SAXParseException &toCatch)
+  {
+    char *message = xercesc::XMLString::transcode(toCatch.getMessage());
+    std::cout << "Exception message is:" << std::endl
+              << message << std::endl;
+    xercesc::XMLString::release(&message);
+    return -1;
+  }
+  catch (const xercesc::XMLException &toCatch)
+  {
+    char *message = xercesc::XMLString::transcode(toCatch.getMessage());
+    std::cout << "Exception message is:" << std::endl
+              << message << std::endl;
+    xercesc::XMLString::release(&message);
+    return -1;
+  }
+  catch (const xercesc::DOMException &toCatch)
+  {
+    char *message = xercesc::XMLString::transcode(toCatch.msg);
+    std::cout << "Exception message is:" << std::endl
+              << message << std::endl;
+    xercesc::XMLString::release(&message);
+    return -1;
+  }
+  // terminate
+  xercesc::XMLPlatformUtils::Terminate();
+  return 0;
+
   return 0;
 } // int main(int argc, char *argv[])
